b7a7a1fd4ba40e128d77eabfbd7751e81907b78a
Yamani AL_tamimi <yamani5006@gmail.com>
2025-01-21 20:03:48 +0300

    pov:adding the clientdetails

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,9 @@
+import ClientDetails from "./pages/client/detaileClients/ClientDetails"
 export default function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return (
+    <>
+    <ClientDetails/>
+    </>
+  )
+  
 }
