86dd4a003364f10474223c19f6266a2e6af0261d
Yamani AL_tamimi <yamani5006@gmail.com>
2025-01-23 19:05:58 +0300

    pov:adding template contain filter+serch

--- src/components/ui/SerachInput.jsx
+++ src/components/ui/SerachInput.jsx
@@ -1,7 +1,10 @@
 import React from 'react'
-
+import { FaSearch } from 'react-icons/fa'
 export default function SerachInput() {
-  return (
-    <div>SerachInput</div>
-  )
+    return (
+        <div className='serach-group flex rounded-lg bg-blue-400 relative w-[250px] border border-gray-300 rounded-lg items-center  '>
+            <input type="text" placeholder='Search' className='search-input border-none outline-none px-2 py-2 w-[250px] rounded ' />
+            <FaSearch className='absolute top-3 right-2' color='gray' />
+        </div>
+      )
 }
